26b43acf4fad1ccefb2ee92956cfa7bfb5f003e4
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-06-20 11:03:23 +0200

    Created skeleton for countryList and header components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Header, CountryList} from "./components";
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        List of countries
+        <Header />
+        <CountryList />
       </div>
     );
   }
---
+++ src/components/country-list/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class CountryList extends Component {
+  render() {
+    return(
+      <div>
+        List of countrys
+      </div>
+    )
+  }
+}
