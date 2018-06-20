34d32d606051c569c96c358fb01f48adb4b265cd
Elin Filipowicz <elin.filipowicz@nordnet.se>
2018-06-20 15:33:26 +0200

    Added route.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import {Header, PageCountryList} from "./components";
 
@@ -6,13 +7,16 @@ import {Header, PageCountryList} from "./components";
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Header />
-        <PageCountryList />
-      </div>
+     <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={PageCountryList} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
-
